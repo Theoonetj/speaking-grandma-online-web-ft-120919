f22e66d9ae0f9ea52765a5d8ff58ff54b14a6bb3
confident-token-1019 <theoonetj@gmail.com>
2019-12-14 05:53:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,12 +13,19 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 
-def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
+def speak_to_grandma(phraseToGrandma)
+  
+  if phraseToGrandma == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.upcase
+    
+    
+  elsif phraseToGrandma == phraseToGrandma.upcase
     return "NO, NOT SINCE 1938!"
+    
+    
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
+  
+  
 end
